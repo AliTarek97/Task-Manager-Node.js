6d2d8e73697171b73a2466f882f6ab7036752860
AliTarek97 <aly_helmy2015@hotmail.com>
2020-01-31 19:04:17 +0200

    Auto-Cropping and Image Formatting

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const auth = require('../middleware/auth');
 const User = require('../models/user');
 const multer = require('multer');
+const sharp = require('sharp');
 
 router.post('/users' , async (req , res) => {
     const user = new User(req.body);
@@ -98,8 +99,10 @@ const upload = multer({
     }
 })
 
-router.post('/users/me/upload' , auth , upload.single('avatar') , async (req,res) => {
-    req.user.avatar = req.file.buffer;
+router.post('/users/me/avatar' , auth , upload.single('avatar') , async (req,res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width:250 , height:250 }).png().toBuffer();
+    
+    req.user.avatar = buffer;
     await req.user.save();
     res.send();
 } , (error , req , res , next) => {
@@ -121,7 +124,7 @@ router.get('/users/:id/avatar', async (req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
     } catch (e) {
         res.status(404).send()
